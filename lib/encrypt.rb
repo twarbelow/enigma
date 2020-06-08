682278aac8d915a2f43c5927c97d09bed36480c0
twarbelow <taija.warbelow@google.com>
2020-06-08 15:17:28 -0700

    imcomplete work

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,3 +1,25 @@
+require './lib/enigma'
+
+msg_file = ARGV[0]
+open = open msg_file
+message = (open.read).chomp
+open.close
+enigma = Enigma.new
+
+encrypted_hash = enigma.encrypt(message)
+
+encrypted_msg = File.new(ARGV[1])
+encrypted_msg{|f| f.write(encrypted_hash[:encryption]) }
+
+
+p encrypted_hash
+p encrypted_msg
+
+
+
+
+
+
 # Add a command line interface for encryption and decryption.
 # You should create a Runner file called encrypt.rb that takes two command line arguments.
 # The first is an existing file that contains a message to encrypt.
