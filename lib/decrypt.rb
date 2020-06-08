90320a68352b092bd82ba1e73fa687aeb5b9884c
twarbelow <taija.warbelow@google.com>
2020-06-08 15:43:46 -0700

    complete cli for decrypt, maybe refactor later

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,11 +1,21 @@
-# Additionally, you should create a Runner file called decrypt.rb that takes four command line arguments.
-# The first is an existing file that contains an encrypted message.
-# The second is a file where your program should write the decrypted message.
-# The third is the key to be used for decryption. The fourth is the date to be used for decryption.
-# In addition to writing the decrypted message to the file,
-# your program should output to the screen the file it wrote to, the key used for decryption,
-# and the date used for decryption.
+require './lib/enigma'
 
-# You should be able to use your CLI like this:
-# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
-# Created 'decrypted.txt' with the key 82648 and date 240818
+if ARGV[0] && ARGV[1] && ARGV[2] && ARGV[3]
+  msg_file = ARGV[0]
+  file = open msg_file
+  message = (file.read).chomp
+  file.close
+
+  enigma = Enigma.new
+  decrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
+  decrypted_msg = File.open(ARGV[1], 'w')
+  decrypted_msg.write(decrypted_hash[:decryption])
+
+  puts "Created #{ARGV[1]} with the key #{ARGV[2]} and date #{ARGV[3]}"
+else
+  puts "wrong number of arguments, please check you have the following:"
+  puts "- encrypted message file name"
+  puts "- file name to write the decrypted message in"
+  puts "- the key to use for decryption"
+  puts "- the date to use for decryption"
+end
